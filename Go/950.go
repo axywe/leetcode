11a9b6be4f18aa60d761d9fbc15f8bda35895839
axywe <axywewastaken@gmail.com>
2024-04-10 22:29:47 +0300

    10 April

---
+++ Go/950.go
@@ -0,0 +1,16 @@
+package main
+
+import "sort"
+
+func deckRevealedIncreasing(deck []int) []int {
+	sort.Ints(deck)
+	q := []int{}
+	for i := len(deck) - 1; i >= 0; i-- {
+		if len(q) <= 1 {
+			q = append([]int{deck[i]}, q...)
+		} else {
+			q = append([]int{deck[i], q[len(q)-1]}, q[:len(q)-1]...)
+		}
+	}
+	return q
+}
